d95d6d9e9aabd7a59fb61827d7bd7fc2eb20eae6
anees <ansmd8374@gmail.com>
2026-09-01 17:16:28 +0530

    Prepare Flask app for Azure App Service deployment

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+import os
 
 app = Flask(__name__)
 
@@ -52,4 +53,5 @@ def add_task():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port, debug=True)
